b29ac1b51a7d907dc41933d53706eca4f9f5675c
Rosario Martinez <111587388+romartinez927@users.noreply.github.com>
2023-09-17 16:25:45 -0300

    cambios class Producto

--- models/Producto.js
+++ models/Producto.js
@@ -1,17 +1,22 @@
 export class Producto {
     #nombre
+    #estado
 
-    constructor({ nombre }) {
+    constructor({ nombre, estado }) {
         if (!nombre || typeof nombre !== 'string') throw new Error("falta el titulo")
         this.#nombre = nombre
-
+        
+        if (typeof estado !== 'boolean') throw new Error("El estado debe ser un booleano")
+        this.#estado = estado
     }
 
     get nombre() { return this.#nombre }
+    get estado() { return this.#estado }
 
     datos() {
         return {
             nombre: this.#nombre,
+            estado: this.#estado,
         }
     }
 }
